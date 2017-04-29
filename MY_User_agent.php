e0e99c69d4508dabd4115dc66d09330d5a81e37b
Jiwan Kim <hahooh@gmail.com>
2017-04-29 15:08:27 +0900

    add tablet detection by extending native user agent library

---
+++ MY_User_agent.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_User_agent extends CI_User_agent
+{
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function is_tablet($key = NULL)
+    {
+        if ($this->is_mobile($key)) {
+
+            $user_agent = trim($_SERVER['HTTP_USER_AGENT']);
+            $is_tablet = preg_match('#.*ipad.*#i', $user_agent);
+            $is_tablet = $is_tablet + preg_match('#Android\' + \'Chrome/[.0-9]* (?!Mobile)#i', $user_agent);
+
+            if ($is_tablet > 0)
+                return true;
+            else
+                return false;
+        } else {
+            return false;
+        }
+    }
+}
